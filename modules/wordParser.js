1d93fd3e84df2a8d6e79b1e068ed708a3d9d0d7c
JastonChamp <142294191+JastonChamp@users.noreply.github.com>
2026-01-17 09:58:03 +0800

    Create modules/wordParser.js with parsing functions

---
+++ modules/wordParser.js
@@ -0,0 +1,15 @@
+// modules/wordParser.js
+
+const parseWord = (word) => {
+    // Function implementation
+};
+
+const getAvailableWords = () => {
+    // Function implementation
+};
+
+const getRandomWord = () => {
+    // Function implementation
+};
+
+export { parseWord, getAvailableWords, getRandomWord };
